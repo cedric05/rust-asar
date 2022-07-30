d6b26fac45eb3f29e5e7cf55c9d3dfc798dd9961
prasanth <kesavarapu.siva@gmail.com>
2022-07-30 12:17:01 +0530

    💄 list asar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use rust_asar::{AsarError, AsarFile};
+use rust_asar::{AsarError, AsarFile, AsarFileEntry};
 mod cli;
 
 fn main() -> Result<(), AsarError> {
@@ -8,8 +8,17 @@ fn main() -> Result<(), AsarError> {
         cli::Commands::List { source } => {
             let asar_file = AsarFile::try_from(source)?;
             let list_files = asar_file.list_files()?;
-            list_files.iter().for_each(|entry| {
-                println!("entry {:?}", entry);
+            list_files.iter().for_each(|entry| match entry {
+                rust_asar::AsarEntry::File(AsarFileEntry {
+                    filename,
+                    offset: _,
+                    size: _,
+                }) => {
+                    println!("{}", filename.to_string_lossy());
+                }
+                rust_asar::AsarEntry::Dir(_directory) => {
+                    // println!("Dir: {}", directory);
+                }
             });
         }
         cli::Commands::Extract { dest, source } => {
